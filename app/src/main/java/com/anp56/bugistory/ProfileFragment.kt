5aa9bdbfad6cdf190a29ee90cdac2fa88af19967
postive12 <48992839+postive12@users.noreply.github.com>
2022-11-11 18:46:22 +0900

    Update profile

--- app/src/main/java/com/anp56/bugistory/ProfileFragment.kt
+++ app/src/main/java/com/anp56/bugistory/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.anp56.bugistory
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +14,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
+import java.sql.Time
 
 class ProfileFragment : Fragment()
 {
@@ -28,12 +30,16 @@ class ProfileFragment : Fragment()
         val binding = FragmentProfileBinding.bind(view)
         userdataCollectionRef.document(Firebase.auth.uid.toString()).get()
             .addOnSuccessListener {
-                binding.phoneTextView.text
+                binding.nameTextView.text = it.get("name").toString()
+                binding.phoneTextView.text = it.get("phone_number").toString()
+                binding.emailTextView.text = it.get("email").toString()
             }
             .addOnFailureListener {
                 Toast.makeText(activity,"프로필 정보를 가져오지 못했습니다.",Toast.LENGTH_SHORT).show();
             }
-
+        binding.settingButton.setOnClickListener {
+            startActivity(Intent(activity,OptionActivity::class.java))
+        }
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
