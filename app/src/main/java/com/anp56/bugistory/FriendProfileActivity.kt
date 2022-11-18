f4d1dbf074337a240a19fbdd0691657315650d7b
postive12 <48992839+postive12@users.noreply.github.com>
2022-11-18 22:07:44 +0900

    프로필 창 업데이트

--- app/src/main/java/com/anp56/bugistory/FriendProfileActivity.kt
+++ app/src/main/java/com/anp56/bugistory/FriendProfileActivity.kt
@@ -1,25 +1,40 @@
 package com.anp56.bugistory
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.anp56.bugistory.databinding.ActivityLoginBinding
-import com.anp56.bugistory.databinding.ActivityProfilefriendBinding
-import com.anp56.bugistory.databinding.FragmentProfileBinding
-import kotlinx.android.synthetic.main.activity_profilefriend.*
+import com.anp56.bugistory.databinding.ActivityProfileFriendBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FriendProfileActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityProfilefriendBinding.inflate(layoutInflater) }
+    private val db = Firebase.firestore
+    private val userCollectionRef = db.collection("userdata")
+    private val binding by lazy { ActivityProfileFriendBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val name = intent.getStringExtra("name")
-        val phonenumber = intent.getStringExtra("phonenumber")
-        val profile = intent.getIntExtra("profile",-1)
-        binding.profileimage.setImageResource(profile)
-        binding.phone.text = phonenumber
-        binding.name.text = name
 
+        binding.friendButton.setImageResource(if(isFriend) R.mipmap.friend_minus_button else R.mipmap.friend_add_button)
+
+        userCollectionRef.document(currentFriendUid).get()
+            .addOnSuccessListener {
+                try {
+                    binding.email.text = it.get("email").toString()
+                    binding.name.text = it.get("name").toString()
+                    binding.phone.text = it.get("pthone_number").toString()
+                }
+                catch (_ : Exception){
+                    //ignored
+                    Toast.makeText(this,"프로필 정보를 불러오는데 실패했습니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,"프로필 정보를 불러오는데 실패했습니다.",Toast.LENGTH_SHORT).show()
+            }
+
+        val profile = intent.getIntExtra("profile",-1)
+        binding.profileImage.setImageResource(profile)
 
         binding.backButton.setOnClickListener{
             super.onBackPressed()
@@ -28,16 +43,15 @@ class FriendProfileActivity : AppCompatActivity() {
         binding.chatButton.setOnClickListener {
         }
 
-        //debug용 스킵 코드
-        //startActivity(
-        //    Intent(this,MainActivity::class.java)
-        //)
-        //finish()
         binding.friendButton.setOnClickListener {
 
         }
-        binding.friendpostButton.setOnClickListener {
+        binding.friendPostButton.setOnClickListener {
 
         }
+    }
+    companion object{
+        lateinit var currentFriendUid : String
+        var isFriend = false
     }
 }
