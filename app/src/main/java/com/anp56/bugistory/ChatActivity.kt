919ce3942a9b0f46d1ca327908486bb45f0f510a
최민영 <choimy1996@gmail.com>
2022-11-25 20:09:42 +0900

    채팅기능 테스트

--- app/src/main/java/com/anp56/bugistory/ChatActivity.kt
+++ app/src/main/java/com/anp56/bugistory/ChatActivity.kt
@@ -1,37 +1,186 @@
 package com.anp56.bugistory
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.anp56.bugistory.chat.ChatAdapter
 import com.anp56.bugistory.chat.ChatData
+import com.anp56.bugistory.chat_list.ChatModel
+import com.anp56.bugistory.chat_list.ChatModel.Comment
 import com.anp56.bugistory.databinding.ActivityChatBinding
-import com.anp56.bugistory.post.PostAdapter
-import com.anp56.bugistory.post.PostData
+import com.anp56.bugistory.friend.FriendData
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.database.R
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_chat.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChatActivity : AppCompatActivity() {
+
+    private val fireDatabase = FirebaseDatabase.getInstance().reference
+    private var chatRoomUid : String? = null
+    private var destinationUid : String? = null
+    private var uid : String? = null
+    private var recyclerView : RecyclerView? = null
     val binding by lazy { ActivityChatBinding.inflate(layoutInflater) }
+
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val chats = mutableListOf<ChatData>()
+        val imageView = send_button
+        val editText = chat_editText
 
-        for (i in 1..10){
-            println("Doing here")
-            var count = i
-            var cont = ""
-            for (j in 0..count){
-                cont += "글이 얼마나 적힐지 테스트"
+        //메시지 보낸 시간
+        val time = System.currentTimeMillis()
+        val dateFormat = SimpleDateFormat("MM월dd일 hh:mm")
+        val curTime = dateFormat.format(Date(time)).toString()
+
+        destinationUid = intent.getStringExtra("destinationUid")
+        uid = Firebase.auth.currentUser?.uid.toString()
+        recyclerView = chat_recycler_view
+
+        imageView.setOnClickListener {
+            Log.d("클릭 시 dest", "$destinationUid")
+            val chatModel = ChatModel()
+            chatModel.users.put(uid.toString(), true)
+            chatModel.users.put(destinationUid!!, true)
+
+            val comment = Comment(uid, editText.text.toString(), curTime)
+            if(chatRoomUid == null){
+                imageView.isEnabled = false
+                fireDatabase.child("chatrooms").push().setValue(chatModel).addOnSuccessListener {
+                    //채팅방 생성
+                    checkChatRoom()
+                    //메세지 보내기
+                    Handler().postDelayed({
+                        println(chatRoomUid)
+                        fireDatabase.child("chatrooms").child(chatRoomUid.toString()).child("comments").push().setValue(comment)
+                        chat_editText.text = null
+                    }, 1000L)
+                    Log.d("chatUidNull dest", "$destinationUid")
+                }
+            }else{
+                fireDatabase.child("chatrooms").child(chatRoomUid.toString()).child("comments").push().setValue(comment)
+                chat_editText.text = null
+                Log.d("chatUidNotNull dest", "$destinationUid")
             }
-            chats.add(
-                ChatData(
-                    "유저 고유 아이디 번호가 들어간다",
-                    if (i%2==0) "상대" else "나",
-                    "이것이 채팅입니다. 이것이 채팅입니다. 이것이 채팅입니다. 이것이 채팅입니다.$count",
-                    i %2 == 0
-                )
-            )
         }
-        val adapter = ChatAdapter(applicationContext, chats)
-        binding.chatRecyclerView.adapter = adapter
+        checkChatRoom()
+    }
+
+
+    private fun checkChatRoom(){
+        fireDatabase.child("chatrooms").orderByChild("users/$uid").equalTo(true)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (item in snapshot.children){
+                        println(item)
+                        val chatModel = item.getValue<ChatModel>()
+                        if(chatModel?.users!!.containsKey(destinationUid)){
+                            chatRoomUid = item.key
+                            send_button.isEnabled = true
+                            recyclerView?.layoutManager = LinearLayoutManager(this@ChatActivity)
+                            recyclerView?.adapter = RecyclerViewAdapter()
+                        }
+                    }
+                }
+            })
+    }
+
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MessageViewHolder>() {
+
+        private val comments = ArrayList<Comment>()
+        private var friend : FriendData? = null
+        init{
+            fireDatabase.child("users").child(destinationUid.toString()).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    friend = snapshot.getValue<FriendData>()
+                    chat_opponent.text = friend?.name
+                    getMessageList()
+                }
+            })
+        }
+
+        fun getMessageList(){
+            fireDatabase.child("chatrooms").child(chatRoomUid.toString()).child("comments").addValueEventListener(object :
+                ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    comments.clear()
+                    for(data in snapshot.children){
+                        val item = data.getValue<Comment>()
+                        comments.add(item!!)
+                        println(comments)
+                    }
+                    notifyDataSetChanged()
+                    //메세지를 보낼 시 화면을 맨 밑으로 내림
+                    recyclerView?.scrollToPosition(comments.size - 1)
+                }
+            })
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+            val view : View = LayoutInflater.from(parent.context).inflate(com.anp56.bugistory.R.layout.item_chat_list, parent, false)
+
+            return MessageViewHolder(view)
+        }
+        @SuppressLint("RtlHardcoded")
+        override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+            holder.textView_message.textSize = 20F
+            holder.textView_message.text = comments[position].message
+            holder.textView_time.text = comments[position].time
+            if(comments[position].uid.equals(uid)){ // 본인 채팅
+                holder.textView_message.setBackgroundResource(com.anp56.bugistory.R.drawable.rightbubble)
+                holder.textView_name.visibility = View.INVISIBLE
+                holder.layout_destination.visibility = View.INVISIBLE
+                holder.layout_main.gravity = Gravity.RIGHT
+            }else{ // 상대방 채팅
+//                Glide.with(holder.itemView.context)
+//                    .load(friend?.profileImageUrl)
+//                    .apply(RequestOptions().circleCrop())
+//                    .into(holder.imageView_profile)
+                holder.textView_name.text = friend?.name
+                holder.layout_destination.visibility = View.VISIBLE
+                holder.textView_name.visibility = View.VISIBLE
+                holder.textView_message.setBackgroundResource(com.anp56.bugistory.R.drawable.leftbubble)
+                holder.layout_main.gravity = Gravity.LEFT
+            }
+        }
+
+        inner class MessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val textView_message: TextView = view.findViewById(com.anp56.bugistory.R.id.messageItem_textView_message)
+            val textView_name: TextView = view.findViewById(com.anp56.bugistory.R.id.messageItem_textview_name)
+            val imageView_profile: ImageView = view.findViewById(com.anp56.bugistory.R.id.messageItem_imageview_profile)
+            val layout_destination: LinearLayout = view.findViewById(com.anp56.bugistory.R.id.messageItem_layout_destination)
+            val layout_main: LinearLayout = view.findViewById(com.anp56.bugistory.R.id.messageItem_linearlayout_main)
+            val textView_time : TextView = view.findViewById(com.anp56.bugistory.R.id.messageItem_textView_time)
+        }
+
+        override fun getItemCount(): Int {
+            return comments.size
+        }
     }
 }
--- app/src/main/java/com/anp56/bugistory/ChatFragment.kt
+++ app/src/main/java/com/anp56/bugistory/ChatFragment.kt
@@ -1,16 +1,32 @@
 package com.anp56.bugistory
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.anp56.bugistory.chat_list.ChatDataList
 import com.anp56.bugistory.chat_list.ChatListAdapter
 import com.anp56.bugistory.chat_list.ChatListData
+import com.anp56.bugistory.chat_list.ChatModel
+import com.anp56.bugistory.friend.FriendData
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ChatFragment : Fragment() {
     companion object {
@@ -18,6 +34,8 @@ class ChatFragment : Fragment() {
             return ChatFragment()
         }
     }
+    private val fireDatabase = FirebaseDatabase.getInstance().reference
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -34,11 +52,84 @@ class ChatFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_chat,container,false)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView_chatlist)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = ChatListAdapter(ChatDataList)
+        recyclerView.adapter = RecyclerViewAdapter()
         val spaceDecoration = RecyclerDecoration(40)
         recyclerView.addItemDecoration(spaceDecoration)
 
         // Inflate the layout for this fragment
         return view//inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.CustomViewHolder>() {
+
+        private val chatModel = ArrayList<ChatModel>()
+        private var uid : String? = null
+        private val destinationUsers : ArrayList<String> = arrayListOf()
+
+        init {
+            uid = Firebase.auth.currentUser?.uid.toString()
+            println(uid)
+
+            fireDatabase.child("chatrooms").orderByChild("users/$uid").equalTo(true).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    chatModel.clear()
+                    for(data in snapshot.children){
+                        chatModel.add(data.getValue<ChatModel>()!!)
+                        println(data)
+                    }
+                    notifyDataSetChanged()
+                }
+            })
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+
+            return CustomViewHolder(LayoutInflater.from(context).inflate(R.layout.item_chat_list, parent, false))
+        }
+
+        inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val imageView: ImageView = itemView.findViewById(R.id.item_image_chat_list)
+            val textView_title : TextView = itemView.findViewById(R.id.item_name_chat_list)
+            val textView_lastMessage : TextView = itemView.findViewById(R.id.item_last_chat)
+        }
+
+        override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+            var destinationUid: String? = null
+            //채팅방에 있는 유저 모두 체크
+            for (user in chatModel[position].users.keys) {
+                if (!user.equals(uid)) {
+                    destinationUid = user
+                    destinationUsers.add(destinationUid)
+                }
+            }
+            fireDatabase.child("users").child("$destinationUid").addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val friend = snapshot.getValue<FriendData>()
+//                    Glide.with(holder.itemView.context).load(friend?.profileImageUrl)
+//                        .apply(RequestOptions().circleCrop())
+//                        .into(holder.imageView)
+                    holder.textView_title.text = friend?.name
+                }
+            })
+            //메세지 내림차순 정렬 후 마지막 메세지의 키값을 가져옴
+            val commentMap = TreeMap<String, ChatModel.Comment>(Collections.reverseOrder())
+            commentMap.putAll(chatModel[position].comments)
+            val lastMessageKey = commentMap.keys.toTypedArray()[0]
+            holder.textView_lastMessage.text = chatModel[position].comments[lastMessageKey]?.message
+
+            //채팅창 선책 시 이동
+            holder.itemView.setOnClickListener {
+                val intent = Intent(context, ChatActivity::class.java)
+                intent.putExtra("destinationUid", destinationUsers[position])
+                context?.startActivity(intent)
+            }
+        }
+        override fun getItemCount(): Int {
+            return chatModel.size
+        }
     }
 }
